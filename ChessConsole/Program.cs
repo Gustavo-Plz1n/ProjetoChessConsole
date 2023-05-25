c2e9be65e1e8b4b4a1e16e0ffb26152cc0224903
Gustavo <plgustavo28@gmail.com>
2023-05-25 10:41:17 -0300

    Criando a classe PosicaoXadrez

--- ChessConsole/Program.cs
+++ ChessConsole/Program.cs
@@ -10,26 +10,9 @@ namespace ChessConsole // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-
-                tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(2, 4));
-
-                Tela.imprimirTabuleiro(tab);
-
-
-
-                Console.ReadLine();
-
-
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Console.WriteLine(pos.toPosicao());
+            Console.WriteLine(pos);
         }
     }
 }
---
+++ ChessConsole/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+
+namespace ChessConsole.xadrez
+{
+    class PosicaoXadrez
+    {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+
+
+
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+
+
+
+
+    }
+}
