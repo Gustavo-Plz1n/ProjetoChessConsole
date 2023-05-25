d97d9b6301bb1a1ac5850ad106399e287bd9493f
Gustavo <plgustavo28@gmail.com>
2023-05-25 08:34:42 -0300

    Criando classe posicao

--- ChessConsole/Program.cs
+++ ChessConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -6,7 +7,13 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao p;
+            p = new Posicao(3, 4);
+            Console.WriteLine("Posição: " + p);
+            Console.ReadLine();
+
+
+
         }
     }
 }
---
+++ ChessConsole/Tabuleiro/Posicao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuleiro
+{
+     class Posicao
+    {
+        public int Linha{get; set; }
+        public int Coluna{get; set; }
+        public Posicao (int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+        public override string ToString()
+        {
+            return Linha + ", " + Coluna;
+        }
+
+
+
+
+    }
+}
